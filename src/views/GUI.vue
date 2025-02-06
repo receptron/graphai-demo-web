502f97b7ce8ded73d5a3b20be5c95634c3fb5a72
isamu <isamu@to-kyo.to>
2025-02-06 10:20:22 +0900

    add node

--- src/views/GUI.vue
+++ src/views/GUI.vue
@@ -176,6 +176,16 @@ export default defineComponent({
       }, null);
     });
 
+    const addNode = () => {
+      const uuid = self.crypto.randomUUID();
+      console.log(uuid);
+      nodes.value.push({
+        nodeId: uuid,
+        type: "aa",
+        position: { x: Math.random() * 200, y: Math.random() * 200 },
+      });
+    };
+
     return {
       updatePosition,
       nodes,
@@ -186,25 +196,29 @@ export default defineComponent({
       svgRef,
       mouseCurrentPosition,
       nearestNode,
+      addNode,
     };
   },
 });
 </script>
 
 <template>
-  <div class="w-screen h-screen relative">
-    <svg x="0" y="0" width="100%" height="100%" class="absolute" ref="svgRef">
-      <Edge v-for="(edge, index) in edgeDataList" :key="index" :from-data="edge.from" :to-data="edge.to" />
-      <Edge v-if="newEdgeData" :from-data="newEdgeData.from" :to-data="newEdgeData.to" />
-    </svg>
-    <Node
-      v-for="(node, index) in nodes"
-      :key="index"
-      :node-data="node"
-      @update-position="(pos) => updatePosition(index, pos)"
-      @new-edge="newEdgeEvent"
-      @new-edge-end="newEdgeEventEnd"
-    />
-    aa{{ mouseCurrentPosition }} {{ nearestNode }}
+  <div>
+    <div class="w-screen h-screen relative">
+      <svg x="0" y="0" width="100%" height="100%" class="absolute" ref="svgRef">
+        <Edge v-for="(edge, index) in edgeDataList" :key="index" :from-data="edge.from" :to-data="edge.to" />
+        <Edge v-if="newEdgeData" :from-data="newEdgeData.from" :to-data="newEdgeData.to" />
+      </svg>
+      <Node
+        v-for="(node, index) in nodes"
+        :key="index"
+        :node-data="node"
+        @update-position="(pos) => updatePosition(index, pos)"
+        @new-edge="newEdgeEvent"
+        @new-edge-end="newEdgeEventEnd"
+      />
+      aa{{ mouseCurrentPosition }} {{ nearestNode }}
+    </div>
+    <div><button @click="addNode">Add node</button></div>
   </div>
 </template>
